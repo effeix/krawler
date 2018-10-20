cafa7a2169d9f71f863172a674670049f1820662
Gustavo Efeiche <gustavo.efeiche@gmail.com>
2018-10-20 14:05:57 -0300

    Simple cURL test

---
+++ krawler/main.cpp
@@ -0,0 +1,75 @@
+// #include "cpr/cpr.h"
+#include <iostream>
+#include "curl/curl.h"
+
+/*
+ * By default, cURL writes responses to stdout. To prevent this and collect the data,
+ * we need to setup a custom callback with the following prototype:
+ *
+ *     size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp);
+ * 
+ * Then we can use the callback to populate a custom buffer with the
+ * contents of the response.
+ * 
+ * CURLOPT_WRITEFUNCTION is used to set the custom callback
+ * CURLOPT_WRITEDATA is used to set the custom buffer we'll write to.
+ * 
+ * Sources:
+ *     https://stackoverflow.com/a/9786295/4922572
+ *     https://curl.haxx.se/libcurl/c/libcurl-tutorial.html
+ */
+
+// Custom callback for cURL
+size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp) {
+    // buffer is a pointer to the response contents
+    ((std::string*)userp)->append((char*)buffer, size * nmemb);
+    std::cout << size*nmemb << std::endl;
+    return size * nmemb;
+}
+
+std::string http_get(std::string url) {
+    CURL *curl;
+    std::string buffer;
+
+    curl = curl_easy_init();
+
+    if(curl) {
+        
+        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+        
+        // cURL uses C strings instead of the string object
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+
+        // If the url suffers a redirection, we tell cURL to follow it
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+        // Set custom callback to be used
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+
+        // Set which variable will be used by the custom callback
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+
+        // Perform the request
+        CURLcode code = curl_easy_perform(curl);
+
+        // Check for failure
+        if(code != CURLE_OK) {
+            fprintf(stderr, "curl failed: %s\n", curl_easy_strerror(code));
+        }
+
+        // Clean all options
+        curl_easy_cleanup(curl);
+    }
+    
+    return buffer;
+}
+
+int main(int argc, char **argv) {
+    std::string response;
+
+    response = http_get("https://www.magazineluiza.com.br/smartphone/celulares-e-smartphones/s/te/tcsp/");
+
+    std::cout << response << std::endl;
+
+    return 0;
+}
